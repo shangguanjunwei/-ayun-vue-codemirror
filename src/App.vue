acdbe43bd55b2e0e4e17f2170d5ffcd802e53d16
shangguanjunwei <shangguanjunwei@we.cn>
2024-06-14 23:38:24 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <AYunCodeMirror v-model="jsonData" lang="sql" />
+    <AYunCodeMirror v-model="jsonData" lang="javascript" />
   </div>
 </template>
 
 <script setup lang="ts">
-import AYunCodeMirror from '@/components/AYunCodemirror';
+import AYunCodeMirror from '@/components/AYunCodemirror/index.vue';
 import { ref } from 'vue';
 
 const jsonData = ref<any>('');
